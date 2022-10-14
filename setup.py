6ae25be31e3e6912530ae9fea017b7b60cfbef7e
Zhaoting Chen <ztchenustc@gmail.com>
2022-10-14 23:24:27 +0100

    restructure

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+#with open("README.md", "r", encoding="utf-8") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="hiimtool",
+    version="0.0.1",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
