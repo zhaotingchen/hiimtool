0a6a6e48580886ffb64fd99a8dcc4f01a61364b6
zhaotingchen <ztchenustc@gmail.com>
2023-07-19 21:36:10 +0100

    add B_0 function

--- tests/test_basic_util.py
+++ tests/test_basic_util.py
@@ -1,4 +1,4 @@
-from hiimtool.basic_util import p2dim,chisq,vfind_scan,vfind_id,Specs,fill_nan,f_21,itr_tnsq_avg,delay_transform,get_conv_mat,himf,cal_himf,cumu_nhi_from_himf,sample_from_dist,busy_function_simple
+from hiimtool.basic_util import p2dim,chisq,vfind_scan,vfind_id,Specs,fill_nan,f_21,itr_tnsq_avg,delay_transform,get_conv_mat,himf,cal_himf,cumu_nhi_from_himf,sample_from_dist,busy_function_simple,busy_function_0
 import pytest
 import numpy as np
 from astropy.cosmology import Planck15,Planck18
@@ -140,3 +140,7 @@ def test_sample_from_dist():
 def test_busy_function_simple():
     xarr = np.linspace(-10,10,101)
     assert np.allclose(busy_function_simple(xarr,2,1,0,0),erf(-xarr**2)+1)
+    
+def test_busy_function_0():
+    xarr = np.linspace(-10,10,101)
+    assert np.allclose(busy_function_0(xarr,4,1,0,0),(erf(-xarr)+1)*(erf(xarr)+1))
